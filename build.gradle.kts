093928ff45e62b5428f22fe3fa6069b8840cb867
wait123-crate <kylepogiplus32@gmail.com>
2025-03-09 21:45:05 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,40 +4,16 @@ plugins {
     alias(libs.plugins.safeargs) apply false
 }
 
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath(libs.plugins.agp)
-        classpath(libs.plugins.kotlin)
-        classpath(libs.plugins.safeargs)
-    }
-}
-
 allprojects {
     repositories {
         google()
         mavenCentral()
     }
- }
+} 
 plugins {
     alias(libs.plugins.agp.app) apply false
     alias(libs.plugins.kotlin) apply false
     alias(libs.plugins.safeargs) apply false
-}
-
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath(libs.plugins.agp)
-        classpath(libs.plugins.kotlin)
-        classpath(libs.plugins.safeargs)
-    }
 }
 
 allprojects {
