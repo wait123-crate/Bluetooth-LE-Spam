9cf472cb1404be76d6db776c3914aa9fee9283ed
wait123-crate <kylepogiplus32@gmail.com>
2025-03-09 21:41:54 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,48 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.agp.app) apply false
     alias(libs.plugins.kotlin) apply false
     alias(libs.plugins.safeargs) apply false
+}
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath(libs.plugins.agp)
+        classpath(libs.plugins.kotlin)
+        classpath(libs.plugins.safeargs)
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+ }
+plugins {
+    alias(libs.plugins.agp.app) apply false
+    alias(libs.plugins.kotlin) apply false
+    alias(libs.plugins.safeargs) apply false
+}
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath(libs.plugins.agp)
+        classpath(libs.plugins.kotlin)
+        classpath(libs.plugins.safeargs)
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
