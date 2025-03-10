e8f2cbb4b9d655fb8aa3f582174531dd2b727da8
wait123-crate <kylepogiplus32@gmail.com>
2025-03-10 11:56:14 +0800

    Update AppContext.kt

--- app/src/main/java/de/simon/dankelmann/bluetoothlespam/AppContext/AppContext.kt
+++ app/src/main/java/de/simon/dankelmann/bluetoothlespam/AppContext/AppContext.kt
@@ -2,21 +2,16 @@ package de.simon.dankelmann.bluetoothlespam
 
 import android.content.Context
 
-class AppContext private constructor() {
-    companion object {
-        // Hold the app context globally
-        private var _context: Context? = null
-        
-        // Method to initialize the AppContext
-        fun initialize(context: Context) {
-            if (_context == null) {
-                _context = context.applicationContext
-            }
+object AppContext {
+    private var _context: Context? = null
+
+    fun initialize(context: Context) {
+        if (_context == null) {
+            _context = context.applicationContext
         }
-        
-        // Method to access the app context
-        fun getContext(): Context {
-            return _context ?: throw IllegalStateException("AppContext not initialized!")
-        }
+    }
+
+    fun getContext(): Context {
+        return _context ?: throw IllegalStateException("AppContext not initialized!")
     }
 }
