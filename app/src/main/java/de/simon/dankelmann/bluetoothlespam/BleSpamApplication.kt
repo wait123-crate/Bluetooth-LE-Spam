0fd11cee9475a02c48b5ac8788184997fe8dfda8
wait123-crate <kylepogiplus32@gmail.com>
2025-03-10 12:05:03 +0800

    Update BleSpamApplication.kt

--- app/src/main/java/de/simon/dankelmann/bluetoothlespam/BleSpamApplication.kt
+++ app/src/main/java/de/simon/dankelmann/bluetoothlespam/BleSpamApplication.kt
@@ -3,7 +3,6 @@ package de.simon.dankelmann.bluetoothlespam
 import android.app.Application
 import de.simon.dankelmann.bluetoothlespam.Interfaces.Services.IBluetoothLeScanService
 import de.simon.dankelmann.bluetoothlespam.Services.BluetoothLeScanService
-import de.simon.dankelmann.bluetoothlespam.AppContext
 
 class BleSpamApplication : Application() {
     lateinit var scanService: IBluetoothLeScanService
@@ -12,7 +11,7 @@ class BleSpamApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        // Initialize AppContext here
+        // Initialize global context
         AppContext.initialize(this)
 
         // Initialize BluetoothLeScanService
